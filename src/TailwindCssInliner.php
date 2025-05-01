a9e35ab935c9a6702053414c62f8ddf931e818f6
LeeRy0y0 <emilleer41@gmail.com>
2025-05-01 16:34:17 +0200

    Første version af Tailwind email inliner

--- src/TailwindCssInliner.php
+++ src/TailwindCssInliner.php
@@ -2,23 +2,14 @@
 namespace TailwindEmail;
 
 use Premailer\Premailer;
-use Illuminate\Support\Facades\Http;
 
 class TailwindCssInliner
 {
-    /**
-     * Inline Tailwind CSS-klasser i en HTML e-mail.
-     *
-     * @param  string  $htmlContent
-     * @return string
-     */
-    public function inlineCss(string $htmlContent)
+    public function inlineCss(string $htmlContent): string
     {
-        // Download Tailwind CSS fra CDN
-        $tailwindCssUrl = 'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css';
-        $cssContent = Http::get($tailwindCssUrl)->body();
+        $cssPath = base_path('resources/css/email-compiled.css');
+        $cssContent = file_get_contents($cssPath);
 
-        // Inline CSS i HTML
         $premailer = new Premailer($htmlContent, [
             'css' => $cssContent,
             'method' => 'html',
